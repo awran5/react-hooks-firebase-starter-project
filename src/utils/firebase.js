9f64c6a303d49f3347d641b707cc164ac9bed106
Ahmed Khalil <awran5@yahoo.com>
2020-01-22 14:12:59 +0200

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,17 +10,17 @@ export { FirebaseContext }
  * so you will need to store these somewhere else.
  */
 export default ({ children }) => {
-	if (!app.apps.length) {
-		app.initializeApp({
-			apiKey: 'xxxxxxxxxxxxxx',
-			authDomain: 'xxxxxxxxxxxxxx',
-			databaseURL: 'xxxxxxxxxxxxxx',
-			projectId: 'xxxxxxxxxxxxxx',
-			storageBucket: 'xxxxxxxxxxxxxx',
-			messagingSenderId: 'xxxxxxxxxxxxxx',
-			appId: 'xxxxxxxxxxxxxx'
-		})
-	}
+  if (!app.apps.length) {
+    app.initializeApp({
+      apiKey: 'xxxxxxxxxxxxxx',
+      authDomain: 'xxxxxxxxxxxxxx',
+      databaseURL: 'xxxxxxxxxxxxxx',
+      projectId: 'xxxxxxxxxxxxxx',
+      storageBucket: 'xxxxxxxxxxxxxx',
+      messagingSenderId: 'xxxxxxxxxxxxxx',
+      appId: 'xxxxxxxxxxxxxx'
+    })
+  }
 
-	return <FirebaseContext.Provider value={app}>{children}</FirebaseContext.Provider>
+  return <FirebaseContext.Provider value={app}>{children}</FirebaseContext.Provider>
 }
