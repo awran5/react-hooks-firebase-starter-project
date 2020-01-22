e461c57124dade8bc5af340161fae4321391b909
Ahmed Khalil <awran5@yahoo.com>
2020-01-22 14:12:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { useSnapshots } from './hooks/useFirebase'
 import HelloWorld from './components/HelloWorld'
 
 function App() {
-	const { loading, data } = useSnapshots('collection name')
+  const { loading, data } = useSnapshots('collection name')
 
-	return (
-		<div className="App">
-			<HelloWorld />
-			{loading ? <h1>Loading...</h1> : data.map(() => <div>{''}</div>)}
-		</div>
-	)
+  return (
+    <div className="App">
+      <HelloWorld />
+      {loading ? <h1>Loading...</h1> : data.map(() => <div>{''}</div>)}
+    </div>
+  )
 }
 
 export default App
