2d6cbfb8e892f054e9cc0b3ac73e8e259a258ca7
Ahmed Khalil <awran5@yahoo.com>
2020-01-22 14:11:05 +0200

    Update useFirebase.js

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -3,64 +3,64 @@ import { FirebaseContext } from '../utils/firebase'
 
 // Get data by 'Snapshot'
 export const useSnapshots = collection => {
-	const firebase = useContext(FirebaseContext)
-	const [ data, setData ] = useState([])
-	const [ loading, setLoading ] = useState(false)
-	const [ error, setError ] = useState('')
+  const firebase = useContext(FirebaseContext)
+  const [ data, setData ] = useState([])
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState('')
 
-	useEffect(
-		() => {
-			const newData = []
-			setLoading(true)
-			const unsubscribe = firebase.firestore().collection(collection).onSnapshot(
-				snapshot => {
-					snapshot.forEach(doc => {
-						newData.push({
-							id: doc.id,
-							...doc.data()
-						})
-					})
-					setData(newData)
-					setLoading(false)
-				},
-				err => {
-					console.log(err)
-					setError(err)
-				}
-			)
-			return () => unsubscribe()
-		},
-		[ firebase, collection ]
-	)
+  useEffect(
+    () => {
+      const newData = []
+      setLoading(true)
+      const unsubscribe = firebase.firestore().collection(collection).onSnapshot(
+        snapshot => {
+          snapshot.forEach(doc => {
+            newData.push({
+              id: doc.id,
+              ...doc.data()
+            })
+          })
+          setData(newData)
+          setLoading(false)
+        },
+        err => {
+          console.log(err)
+          setError(err)
+        }
+      )
+      return () => unsubscribe()
+    },
+    [ firebase, collection ]
+  )
 
-	return { loading, error, data }
+  return { loading, error, data }
 }
 
-// Get data by 'Doc' id
+// Get data by 'Doc' ID
 export const useDocById = (collection, id) => {
-	const firebase = useContext(FirebaseContext)
-	const [ data, setData ] = useState([])
-	const [ loading, setLoading ] = useState(false)
+  const firebase = useContext(FirebaseContext)
+  const [ data, setData ] = useState([])
+  const [ loading, setLoading ] = useState(false)
 
-	useEffect(
-		() => {
-			setLoading(true)
-			const unsubscribe = firebase
-				.firestore()
-				.collection(collection)
-				.doc(id)
-				.get()
-				.then(doc => {
-					setData({ id, ...doc.data() })
-					setLoading(false)
-				})
-				.catch(err => {
-					console.log(err)
-				})
-			return () => unsubscribe
-		},
-		[ firebase, collection, id ]
-	)
+  useEffect(
+    () => {
+      setLoading(true)
+      const unsubscribe = firebase
+        .firestore()
+        .collection(collection)
+        .doc(id)
+        .get()
+        .then(doc => {
+          setData({ id, ...doc.data() })
+          setLoading(false)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      return () => unsubscribe
+    },
+    [ firebase, collection, id ]
+  )
 
-	return { loading, data }
+  return { loading, data }
 }
